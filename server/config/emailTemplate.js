017c11ea168adb6ae18508f2784cfea7413de6d4
Aditya-Kumar-Thakur <adityaslg28@gmail.com>
2025-10-18 21:36:14 +0530

    first-commit

---
+++ server/config/emailTemplate.js
@@ -0,0 +1,5 @@
+export const EMAIL_VERIFY_TEMPLATE = `<td align="center" class="esd-block-image es-p5t es-p5b" style="font-size:0">
+  <a target="_blank">
+    <img src="https://tlr.stripocdn.email/content/guids/CABINET_dd354a98a803b60e2f0411e893c82f56/images/23891556799905703.png" alt="" width="175" style="display:block">
+  </a>
+</td>`;
